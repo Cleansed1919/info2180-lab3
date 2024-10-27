93f36013d5272a64674b1b48c3bf5b6fd4bedab0
bread <wgary283@gmail.com>
2024-10-27 16:00:39 -0600

    Cleaned up tic-tac-toe.js, More console messages

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,23 +4,39 @@ let state = ["","","","","","","","",""];
 let stateIndex = 0;
 let isWinner = "";
 
-winner = function() {
-    if (state[0] === state[1] && state[1] === state[2] && state[2] !== "") 
-        {return state[0];}
-    else if (state[3] === state[4] && state[4] === state[5] && state[5] !== "") 
-        {return state[3];}
-    else if (state[6] === state[7] && state[7] === state[8] && state[8] !== "") 
-        {return state[6];}
-    else if (state[0] === state[3] && state[3] === state[6] && state[6] !== "") 
-        {return state[0];}
-    else if (state[1] === state[4] && state[4] === state[7] && state[7] !== "") 
-        {return state[1];}
-    else if (state[2] === state[5] && state[5] === state[8] && state[8] !== "") 
-        {return state[2];}
-    else if (state[0] === state[4] && state[4] === state[8] && state[8] !== "") 
-        {return state[0];}
-    else if (state[2] === state[4] && state[4] === state[6] && state[6] !== "") 
-        {return state[2];}
+isWinner = function() {
+    if (state[0] === state[1] && state[1] === state[2] && state[2] !== "") {
+        console.log("Row Clear!");
+        return state[0];
+    }
+    else if (state[3] === state[4] && state[4] === state[5] && state[5] !== "") {
+        console.log("Row Clear!");
+        return state[3];
+    }
+    else if (state[6] === state[7] && state[7] === state[8] && state[8] !== "") {
+        console.log("Row Clear!");
+        return state[6];
+    }
+    else if (state[0] === state[3] && state[3] === state[6] && state[6] !== "") {
+        console.log("Column Clear!");
+        return state[0];
+    }
+    else if (state[1] === state[4] && state[4] === state[7] && state[7] !== "") {
+        console.log("Column Clear!");
+        return state[1];
+    }
+    else if (state[2] === state[5] && state[5] === state[8] && state[8] !== "") {
+        console.log("Column Clear!");
+        return state[2];
+    }
+    else if (state[0] === state[4] && state[4] === state[8] && state[8] !== "") {
+        console.log("Diagonal Clear!");
+        return state[0];
+    }
+    else if (state[2] === state[4] && state[4] === state[6] && state[6] !== "") {
+        console.log("Diagonal Clear!");
+        return state[2];
+    }
     else {
         return false;
     }
@@ -50,6 +66,7 @@ setGame = function() {
                     cell.innerHTML = "O";
                     position = Number(cell.getAttribute("id"));
                     state[`${position}`] = "O";
+                    console.log("O in position: ", position);
     
                 }
                 else {
@@ -57,16 +74,14 @@ setGame = function() {
                     cell.innerHTML = "X";
                     position = Number(cell.getAttribute("id"));
                     state[`${position}`] = "X";
+                    console.log("X in position: ", position);
                 }
-                console.log("A cell was clicked!");
             }
-            isWinner = winner();
-            if (isWinner != false) {
-                status.textContent = `Congratulations! ${isWinner} is the Winner!`;
+            if (isWinner() != false) {
+                status.textContent = `Congratulations! ${isWinner()} is the Winner!`;
                 status.classList.add("you-won");
                 console.log("A winner has been decided.");
                 console.log("Game end.");
-                console.log(state);
             }
         }
         stateIndex++;
@@ -74,7 +89,7 @@ setGame = function() {
             cell.classList.add("hover");
         }
         cell.onmouseout = function() {
-        cell.classList.remove("hover");
+            cell.classList.remove("hover");
         }
     }
 }
@@ -87,18 +102,18 @@ document.addEventListener("DOMContentLoaded", function() {
         state = ["","","","","","","","",""];
             let cells = document.getElementById("board");
             let status = document.getElementById("status");
-            isWinner = false;
             for (let cell of cells.children) {
                 if (cell.classList.contains("O")) {
-                cell.classList.remove("O");
+                    cell.classList.remove("O");
                 }
-            if (cell.classList.contains("X")) {
-                cell.classList.remove("X");
+                if (cell.classList.contains("X")) {
+                    cell.classList.remove("X");
                 }   
             cell.innerHTML = "";
             status.textContent = "Move your mouse over a square and click to play an X or an O";
-            status.classList.remove("you-won");
+            status.classList.remove("you-won");  
             }
+            console.log("Reset Board");
         };
     } 
     
